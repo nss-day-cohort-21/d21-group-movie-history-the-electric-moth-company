4f2224cf8756393a5d21d5b3d68cdfd2c907c93a
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-24 15:42:17 -0500

    connected search

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,24 +8,30 @@ var login = require('./login');
 
 console.log("stuf", fire, movie, card, users);
 
-
 var movieObject = {};
 fire.getDBRef();
-var userInput = "Finding Nemo";
-movie.getSearch(userInput)
-.then((results) => {
-  console.log("results", results.length);
-  for (var i = 0; i < results.length; i++) {
-    let item = results[i];
-    movieObject[i] = {
-      title: item.title,
-      year: item.release_date,
-      poster: `http://image.tmdb.org/t/p/w342${item.poster_path}`,
-      overview: item.overview,
-      movieID: item.id,
-      rating: 0,
-      watched: false
-    };
+
+$("#search").on('keyup', function (pushEnter) {
+  if (pushEnter.which === 13) {
+    $('.row').empty();
+    let userVal = $("#search").val();
+    movie.getSearch(userVal)
+    .then((results) => {
+      console.log("results", results.length);
+      for (var i = 0; i < results.length; i++) {
+        let item = results[i];
+        movieObject[i] = {
+          title: item.title,
+          year: item.release_date,
+          poster: `http://image.tmdb.org/t/p/w342${item.poster_path}`,
+          overview: item.overview,
+          movieID: item.id,
+          rating: 0,
+          watched: false
+        };
+      }
+      card.createCard(movieObject);
+    });
+    $("#search").val("");
   }
-  card.createCard(movieObject);
 });
