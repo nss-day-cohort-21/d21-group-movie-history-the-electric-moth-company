546bcb725d814071bdc3a2c6b5781069859df730
Michael Lindstrom <michaellindstr@gmail.com>
2017-08-25 00:18:00 -0500

    created function to mark watched and if not in firebase then add to firebase

--- javascripts/handlers.js
+++ javascripts/handlers.js
@@ -23,7 +23,7 @@ var handlers = {
 
   watchList: function(item) {
     $(`#plus${item.movieID}`).on("click", (e) => {
-      // fire.addToWatchList(item);
+      e.preventDefault();
       fire.returnWatchList()
       .then((watchList) => {
         let movieIDArr = [];
@@ -32,9 +32,31 @@ var handlers = {
           let thisMovie = watchList[witem];
           movieIDArr.push(thisMovie.movieID);
         });
-        console.log("movieIDArr", movieIDArr);
         if (movieIDArr.indexOf(item.movieID) === -1) {
           fire.addToWatchList(item);
+        }
+      });
+    });
+  },
+
+  markWatched: function(item) {
+    $(`#watch${item.movieID}`).on("click", (e) => {
+      e.preventDefault();
+      fire.returnWatchList()
+      .then((watchList) => {
+        let uglyID;
+        let watchListKeys = Object.keys(watchList);
+        $(watchListKeys).each((windex, witem) => {
+          let thisMovie = watchList[witem];
+          if (thisMovie.movieID ===  item.movieID) {
+            uglyID = watchListKeys[windex];
+          }
+        });
+        if (uglyID === undefined) {
+          item.watched = true;
+          fire.addToWatchList(item);
+        } else {
+          fire.markWatched(item, uglyID);
         }
       });
     });
