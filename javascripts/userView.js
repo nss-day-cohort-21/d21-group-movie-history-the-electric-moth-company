71e3402ed413576544a39766d87b10ecd3e33584
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-08-25 13:21:03 -0500

    userview

--- javascripts/userView.js
+++ javascripts/userView.js
@@ -2,9 +2,16 @@
 
 console.log("User View.js, YO!");
 var fbCall = require('./firebaseCalls.js');
+var cardCreation = require('./cardCreation.js');
 
 $("#my-movies").on("click", function(){
+    $('.row').empty();
     $("#userView").show();
     $("#searchView").hide();
-    console.log(fbCall.returnWatchList());
+    fbCall.returnWatchList()
+    .then(function(data){
+        cardCreation.createCard(data);
+        console.log(cardCreation);
+        console.log(data);
+    // });
 });
